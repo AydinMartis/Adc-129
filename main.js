ad665546a34a90ab25bd9a706c82498686884a64
AydinMartis <90508118+AydinMartis@users.noreply.github.com>
2021-11-12 14:30:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+song="";
+leftWristX=0;
+leftWristY=0;
+
+rightWristX=0;
+rightWristY=0;
+
+scorekeypoints=0;
+
+
+function setup ()
+{
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+
+
+}
+
+function modelloaded ()
+{
+    console.log('Posenet is initialised!');
+}
+
+function gotposes (results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        scorekeypoints=results[0].pose.keypoints[9].score;
+        console.log("scoreleftWrist = "+scorekeypoints);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+"leftWristY = "+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+"rightWristY = "+rightWristY);
+    }
+
+}
+
+function draw ()
+
+{
+    image(video,0,0,600,500);
+    fill("red");
+   stroke("red");
+
+   if(scorekeypoints>0.2)
+   {
+    circle(leftWristX,leftWristY,20);
+    number_leftWristY= Number(leftWristY);
+    remove_decimal= floor(number_leftWristY);
+    volume= remove_decimal/500;
+    document.getElementById('volume').innerHTML="Volume ="+volume;
+    song.setVolume(volume);
+
+   }
+   
+   
+
+}
+
+function preload ()
+{
+    song=loadSound("music.mp3");
+}
+
+function play ()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
